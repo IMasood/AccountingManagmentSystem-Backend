00b5e838e7726ea46acd8046fe977230eadaf822
DESKTOP-JKB24PC\Ali <aliraza.chandyo1@gmail.com>
2023-09-23 14:50:00 +0500

    init commit

---
+++ src/test/java/com/example/accountingmanagementsystem/AccountingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accountingmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
