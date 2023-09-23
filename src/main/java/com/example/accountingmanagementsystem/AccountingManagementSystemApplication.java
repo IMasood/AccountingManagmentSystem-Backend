a0beff4e8b2a38aec856c25bff3c4d33c6d59430
DESKTOP-JKB24PC\Ali <aliraza.chandyo1@gmail.com>
2023-09-23 16:16:57 +0500

    init

---
+++ src/main/java/com/example/accountingmanagementsystem/AccountingManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.accountingmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountingManagementSystemApplication.class, args);
+    }
+
+}
